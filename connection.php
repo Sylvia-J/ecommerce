716e350769931602e2335bda2997737a0ca396e1
Sylvia-J <44234085+Sylvia-J@users.noreply.github.com>
2018-11-06 11:07:21 +0300

    Update connection.php

--- connection.php
+++ connection.php
@@ -1,7 +1,7 @@
 <?php
 define('DB_SERVER', 'localhost');
 define('DB_USERNAME', 'root');
-define('DB_PASSWORD', '36987412');
+define('DB_PASSWORD', '');
 define('DB_DATABASE', 'e-commerce');
 
 $db = mysqli_connect(DB_SERVER,DB_USERNAME,DB_PASSWORD,DB_DATABASE);
