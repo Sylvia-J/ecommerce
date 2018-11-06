55ac453c64ffeff0e6f28f567e004c09be0bddeb
mcanthony98 <36364125+mcanthony98@users.noreply.github.com>
2018-11-06 13:00:40 +0300

    Update delete.php

--- delete.php
+++ delete.php
@@ -4,4 +4,12 @@ require "connection.php";
 $id = $_GET["user_id"];
 
 $query = "DELETE FROM users WHERE user_id = $id";
+if ($conn->query($query)===TRUE){
+		
+		
+		echo "<script type=\"text/javascript\">window.alert('Account deleted successfully.');
+window.location.href = 'home.php';</script>";
+	}else{
+		print "Failed 1<br/>" . $conn->error;
+	}
 ?>
